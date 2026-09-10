bdc2cc31e732bf49271e549efefabdd9351affe9
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-09-10 07:55:19 +0200

    Prepared.

--- declarations/source/library/index.d.ts
+++ declarations/source/library/index.d.ts
@@ -1,5 +1,5 @@
 export { ERROR_NOT_STANDARDIZED_MESSAGE, ERROR_NOT_STANDARDIZED_STATUS, } from "../comment-variables/items.js";
 export { successFalse, successTrue, typeError, typeWarning, } from "./constants/index.js";
 export { makeSuccessFalseTypeError, makeSuccessFalseTypeWarning, makeErrorTypeError, makeErrorTypeWarning, showVSCodeError, treatVSCodeErrors, logCLIError, escapeRegex, trimStringWithLimit, spaceOutESLintMessage, highlightFirstLineOfCode, pathExistsAsAFile, pathExistsAsADirectory, } from "./utilities/index.js";
-export { atHandTemplateFunction } from "../../comment-variables/template-functions/at-hand.js";
+export { atHandTemplateFunction } from "../../comment-variables/template-functions/en/at-hand.js";
 export { commentVariablesData } from "../comment-variables/public.js";
